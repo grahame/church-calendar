1ed023ad2e56f4d2644c7c67af6be7ad4258e948
Grahame Bowland <grahame@bowland.au>
2023-11-22 03:03:19 +0800

    flexible after pentecost time

---
+++ nth.ts
@@ -0,0 +1,40 @@
+// credit: https://stackoverflow.com/a/20426113
+
+const special = [
+    "zeroth",
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+    "thirteenth",
+    "fourteenth",
+    "fifteenth",
+    "sixteenth",
+    "seventeenth",
+    "eighteenth",
+    "nineteenth",
+];
+const deca = ["twent", "thirt", "fort", "fift", "sixt", "sevent", "eight", "ninet"];
+
+export const nthify = (n: number): string => {
+    if (n > 99) {
+        throw new Error("nthify only supports numbers up to 99");
+    }
+    if (n < 20) {
+        return special[n];
+    }
+    if (n % 10 === 0) {
+        return deca[Math.floor(n / 10) - 2] + "ieth";
+    }
+    return deca[Math.floor(n / 10) - 2] + "y-" + special[n % 10];
+};
+
+export const capitalize = (s: string) => s && s[0].toUpperCase() + s.slice(1);
--- western/festivals/pentecost.ts
+++ western/festivals/pentecost.ts
@@ -1,4 +1,31 @@
 import { Denomination, Festival, LiturgicalYearContext, ObservationLevel } from "../../calendar.ts";
+import { capitalize, nthify } from "../../nth.ts";
+import { Temporal } from "../../temporal.ts";
+
+const sundays_after_pentecost = () => {
+    const sundays: Festival[] = [];
+    for (let sunday_after = 2; sunday_after <= 30; sunday_after++) {
+        const nth = capitalize(nthify(sunday_after));
+        sundays.push({
+            slug: `sunday-after-pentecost-${sunday_after}`,
+            name: `${nth} Sunday after Pentecost`,
+            observances: [
+                {
+                    denominations: [Denomination.ANG_AU],
+                    level: ObservationLevel.NON_DISPLACABLE,
+                    dates: (ctxt: LiturgicalYearContext) => {
+                        const dt = ctxt.pentecost.add({ days: 7 * sunday_after });
+                        if (Temporal.PlainDate.compare(dt, ctxt.last_day) === 1) {
+                            return [];
+                        }
+                        return [dt];
+                    },
+                },
+            ],
+        });
+    }
+    return sundays;
+};
 
 const Festivals: Festival[] = [
     {
@@ -29,6 +56,7 @@ const Festivals: Festival[] = [
             },
         ],
     },
+    ...sundays_after_pentecost(),
 ];
 
 export default Festivals;
