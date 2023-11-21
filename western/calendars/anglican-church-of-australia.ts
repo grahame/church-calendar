fc97e494d6d7d21d5a5e2cef2d7f911cbb6e8e4d
Grahame Bowland <grahame@bowland.au>
2023-11-22 06:13:10 +0800

    some docs

--- western/calendars/anglican-church-of-australia.ts
+++ western/calendars/anglican-church-of-australia.ts
@@ -33,6 +33,14 @@ export const calendar = (year: number): ResolvedCalendar => {
         }
     };
 
+    // this seems complicated... because it is. we really have two calendars tangled up:
+    // one running from the secular 'calendar' (Jan 1 - Dec 31) and the other running
+    // from the Church calendar (Advent - Christ the King). We need to resolve the
+    // full year before in order to be able to resolve the current liturgical year,
+    // because some calendar-based observances from the previous year might fall in this
+    // liturgical year. This could possibly cascade back even further when the optional
+    // placement of liturgical dates is taken into account, but we can probably ignore
+    // that for now.
     const ctxts = [make_liturgical_year_context(year - 1), make_liturgical_year_context(year)];
     pack_principal_years(ctxts);
     pack_festival_year(ctxts);
