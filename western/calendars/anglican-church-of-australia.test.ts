7f35139dc0f5189a566f0065bd3c04872631a70c
Grahame Bowland <grahame@bowland.au>
2023-11-22 08:12:07 +0800

    basic test that calendar doesn't crash for a wide range of years

---
+++ western/calendars/anglican-church-of-australia.test.ts
@@ -0,0 +1,8 @@
+import { assert } from "../../testfuncs.ts";
+import { calendar } from "./anglican-church-of-australia.ts";
+
+Deno.test("Anglican Church of Australia calendar produces output", () => {
+    for (let year = 1900; year < 2100; year++) {
+        assert(calendar(year).length > 0);
+    }
+});
