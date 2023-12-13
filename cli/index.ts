8d9b2cab884577c1f42f6867bac18ad92e4253be
Grahame Bowland <grahame@bowland.au>
2023-12-13 21:35:12 +0800

    wip

--- cli/index.ts
+++ cli/index.ts
@@ -1,8 +1,10 @@
+import { Temporal } from "npm:@js-temporal/polyfill@0.4.4";
 import { calendar as aca_calendar } from "../western/calendars/anglican-church-of-australia.ts";
-import { Temporal } from "../temporal.ts";
+import { getLiturgicalYear } from "../western/liturgicalyear.ts";
 
 const main = () => {
-    const year = Number.parseInt(Deno.args[0]) || new Date().getFullYear();
+    // hard-coded timezone is to work around Chromium/node bug on macOS 14
+    const year = getLiturgicalYear(Temporal.Now.plainDateISO("Australia/Perth"));
     const [ctxt, placed_events] = aca_calendar(year);
     console.log(
         `Calendar for liturgical year beginning ${ctxt.first_day.toString()}, ending ${ctxt.last_day.toString()}:`
