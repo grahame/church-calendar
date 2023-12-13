01a418bf24351063696b6a22b50055a5f3b28554
Grahame Bowland <grahame@bowland.au>
2023-12-13 18:39:48 +0800

    swap libs to version that works with dnt

--- libs.ts
+++ libs.ts
@@ -1,5 +1,5 @@
-export * as path_mod from "https://deno.land/std@0.207.0/path/mod.ts";
-export * as fs_mod from "https://deno.land/std@0.207.0/fs/mod.ts";
-export * as glob_mod from "https://deno.land/std@0.207.0/path/glob.ts";
-export * as assert_mod from "https://deno.land/std@0.207.0/assert/mod.ts";
-export { assert, assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts";
+export * as path_mod from "https://deno.land/std@0.203.0/path/mod.ts";
+export * as fs_mod from "https://deno.land/std@0.203.0/fs/mod.ts";
+export * as glob_mod from "https://deno.land/std@0.203.0/path/glob.ts";
+export * as assert_mod from "https://deno.land/std@0.203.0/assert/mod.ts";
+export { assert, assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
